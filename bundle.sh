f97c49d946fbc918ef6b0ff6c00287ac55e26b63
paulw <paul.warwicker@ge.com>
2021-01-13 20:12:23 +0000

    fix loop in describe

--- bundle.sh
+++ bundle.sh
@@ -371,7 +371,7 @@ logs() {
 describe() {
         sep ${FUNCNAME[0]}
 
-		for type in deploy,svc,pods,daemonsets,pvc,cronjobs,jobs,configmaps,secrets,ingress,role,rolebinding,sa; do
+		for type in deploy svc pods daemonsets pvc cronjobs jobs configmaps secrets ingress role rolebinding sa; do
 			kubectl get namespace --no-headers 2>/dev/null | awk '{ print $1 }' | while read ns; do
 					sep2 "$type -- $ns" ${FUNCNAME[0]}
 					kubectl describe $type -n $ns 2>&1
